b8216a17b44e658c4e135e300a3b8107fea62731
Yuting Chou <ytingchou@gmail.com>
2016-11-27 17:50:25 +1100

    Implemented linkIt.

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -7,6 +7,27 @@
 
  (function($) {
   $.fn.linkIt = function(options) {
+    // Default Settings
+    var settings = $.extend({
+      href:   null,
+      text:   null,
+      target: '_self'
+    }, options);
 
+    // Validation
+    if (settings.href == null) {
+      console.log('You need a href option for Linkit to work')
+      return this;
+    }
+
+    return this.each(function() {
+      var object = $(this);
+
+      if (settings.text == null) {
+        settings.text = object.text();
+      }
+
+      object.wrap('<a target="' + settings.target + '" href="' + settings.href + '"></a>').text(settings.text);
+    });
   }
  }(jQuery));
